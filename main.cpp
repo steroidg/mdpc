267d104d901c59db2f4184468219f121f3272689
Dinan Yin <steroidg@gmail.com>
2013-06-29 22:46:43 +1000

    created loop and quit event detection

--- main.cpp
+++ main.cpp
@@ -6,14 +6,27 @@ using namespace std;
 
 int main( int argc, char *argv[] )
 {
-    
+	const int SCREEN_WIDTH	= 1024;
+	const int SCREEN_HEIGHT = 480;
+	const int SCREEN_BPP = 32;
+	
+	bool quit = false;
+	SDL_Event event;
+    SDL_Surface *screen = NULL;
+	
     SDL_Init( SDL_INIT_EVERYTHING );
-    
-    SDL_Surface *screen = SDL_SetVideoMode( 640, 480, 24, SDL_HWSURFACE );
+    screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_HWSURFACE );
 
-    // Just keep the main window open for 5 seconds then close.
-    SDL_Delay( 5000 );
+	while ( quit == false ) {
+		while( SDL_PollEvent( &event ) ) {
+			if( event.type == SDL_QUIT ) {
+				printf ("%s %d Quit event decected.\n", __PRETTY_FUNCTION__, __LINE__);
+				quit = true;
+			}
+		}
+	}
     
+    SDL_FreeSurface ( screen );
     SDL_Quit();
     
 	return 0;
