155f594f16185bd99f5b3ddc5996887486c0d7fc
Dinan Yin <steroidg@gmail.com>
2013-07-02 21:07:04 +1000

    testing out giggle

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SDL.h>
 #include <iostream>
+#include "player_character.hpp"
 
 
 using namespace std;
@@ -12,10 +13,20 @@ int main( int argc, char *argv[] )
 	
 	bool quit = false;
 	SDL_Event event;
-    SDL_Surface *screen = NULL;
+	SDL_Surface *screen = NULL;
+	SDL_Surface *player = NULL;
 	
     SDL_Init( SDL_INIT_EVERYTHING );
-    screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_HWSURFACE );
+	screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_HWSURFACE );
+	player = SDL_CreateRGBSurface(0, 60, 60, 32, 0, 0, 0, 0);
+	if ( player == NULL) {
+		printf ("%s %d ERROR: failed to create player surface.\n", __PRETTY_FUNCTION__, __LINE__);
+		return 1;
+	}
+	
+	PlayerCharacter pl( 70, 70 ); 
+	int poo = pl.get_h();
+	printf ("%s %d h= %d\n", __PRETTY_FUNCTION__, __LINE__, poo);
 
 	while ( quit == false ) {
 		Uint32 bg_colour = SDL_MapRGB(screen->format, 0, 255, 255);
@@ -24,12 +35,13 @@ int main( int argc, char *argv[] )
 				return 1;
 		} 
 		
-		Uint32 block_colour = SDL_MapRGB(screen->format, 255, 0, 255);
-		SDL_Rect block_location = { 0, 0, 60, 60 };
-		if ( SDL_FillRect(screen, &block_location , block_colour) == -1 ) {
-				printf ("%s %d ERROR: failed to fill background colour.\n", __PRETTY_FUNCTION__, __LINE__);
+		Uint32 block_colour = SDL_MapRGB(player->format, 0, 0, 255);
+		SDL_Rect block_location = { 0, 60, 60, 60 };
+		if ( SDL_FillRect(player, &player->clip_rect , block_colour) == -1 ) {
+				printf ("%s %d ERROR: failed to fill player colour.\n", __PRETTY_FUNCTION__, __LINE__);
 				return 1;
 		} 
+		SDL_BlitSurface( player, NULL, screen, &block_location );
 		
 		if ( SDL_Flip(screen) == -1 ) {
 				printf ("%s %d ERROR: failed to flip.\n", __PRETTY_FUNCTION__, __LINE__);
@@ -44,8 +56,9 @@ int main( int argc, char *argv[] )
 		}
 	}
     
-    SDL_FreeSurface ( screen );
-    SDL_Quit();
+	SDL_FreeSurface ( screen );
+	SDL_FreeSurface ( player );
+	SDL_Quit();
     
 	return 0;
 }
