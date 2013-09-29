c0e2f1b97d5e37a5b921475b86f491336be5e1fa
Billy <billy@splort.riverside.csiro.au>
2013-09-29 22:42:40 +1000

    [#57571910] initial commit of basic_drawable_object.hpp

---
+++ basic_drawable_object.hpp
@@ -0,0 +1,25 @@
+#include <SDL.h>
+
+class BasicDrawableObject
+{
+public:
+    BasicDrawableObject ( int x, int y, int w, int h, int bpp );
+    ~BasicDrawableObject ();
+    SDL_Surface * get_surface ();
+    int get_x ();
+    int get_y ();
+    int get_w ();
+    int get_h ();
+    void set_x ( int x );
+    void set_y ( int y );
+    void set_w ( int w );
+    void set_h ( int h );
+
+protected:
+    SDL_Surface * surface;
+    int x;
+    int y;
+    int w;
+    int h;
+    int bpp;
+};
