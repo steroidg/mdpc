3975afe925979a20d2b873b955de9a25e82fabd2
Billy <billy@splort.riverside.csiro.au>
2014-02-16 22:47:03 +1100

    initial commit of renderer.cpp and renderer.hpp

---
+++ renderer.hpp
@@ -0,0 +1,14 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+#include "mdpc.hpp"
+
+class Renderer
+{
+    public:
+        /** Constructor */
+        Renderer ();
+        /** Destructor */
+        ~Renderer ();
+};
+
+#endif
